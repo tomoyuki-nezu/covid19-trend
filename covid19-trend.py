9dd3f296c7d494d69a4b3cdb1858628d34c08c99
Tomoyuki Nezu <tomoyuki@genemagic.com>
2020-04-30 09:22:15 +0900

    Source support to standard input.

--- covid19-trend.py
+++ covid19-trend.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+
 def get_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--source', type=str, default="https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv", help="CSV data url or file.")
@@ -19,13 +20,17 @@ def get_args():
 	parser.add_argument('--ma1', type=int, default=5, help="MA1 length.")
 	parser.add_argument('--ma2', type=int, default=25, help="MA2 length.")
 	parser.add_argument('--ma3', type=int, default=75, help="MA3 length.")
+	parser.add_argument('--bbp', type=int, default=20, help="BB period.")
+	parser.add_argument('--title', type=str, default="COVID19 Trend analysis.", help="Graph title.")
 	args = parser.parse_args()
 	return args
 
-def main():
-	args = get_args()
+def process(args):
+	if args.source is None:
+		df = pd.read_csv(sys.stdin)
+	else:
+		df = pd.read_csv(args.source)
 
-	df = pd.read_csv(args.source)
 	df['公表_年月日'] = pd.to_datetime(df['公表_年月日'], format="%Y-%m-%d")
 
 	tmp_df = df[['公表_年月日']].copy()
@@ -47,17 +52,38 @@ def main():
 	macd_df['macd'] = macd_df['ema_fast'] - macd_df['ema_slow']
 	macd_df['signal'] = macd_df['macd'].ewm(span=args.signal).mean()
 
+	bb_df = pd.DataFrame()
+	bb_data = count_by_date_df['count'].values.tolist()
+	bb_df['date'] = count_by_date_df['date']
+	bb_df['close'] = count_by_date_df['count']
+	bb_df['mean'] = bb_df['close'].rolling(window=args.bbp).mean()
+	bb_df['std'] = bb_df['close'].rolling(window=args.bbp).std()
+	bb_df['bb_up1'] = bb_df['mean'] + bb_df['std']
+	bb_df['bb_up2'] = bb_df['mean'] + bb_df['std']*2
+	bb_df['bb_low1'] = bb_df['mean'] - bb_df['std']
+	bb_df['bb_low2'] = bb_df['mean'] - bb_df['std']*2
+
 	fig, (fig1, fig2) = plt.subplots(2, 1, gridspec_kw = {'height_ratios':[3, 1]})
 
 	fig1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-	fig1.set_title("Moving Average")
-	fig1.text(0.05, 0.90, f'MA1: {args.ma1}', transform=fig1.transAxes)
-	fig1.text(0.05, 0.85, f'MA2: {args.ma2}', transform=fig1.transAxes)
-	fig1.text(0.05, 0.80, f'MA3: {args.ma3}', transform=fig1.transAxes)
+	fig1.set_title("Moving Average and Bollinger Bands")
+	fig1.text(0.05, 0.90, f'Title: {args.title}', transform=fig1.transAxes)
+	fig1.text(0.05, 0.80, f'MA1: {args.ma1}', transform=fig1.transAxes)
+	fig1.text(0.05, 0.75, f'MA2: {args.ma2}', transform=fig1.transAxes)
+	fig1.text(0.05, 0.70, f'MA3: {args.ma3}', transform=fig1.transAxes)
 	fig1.bar(macd_df['date'], macd_df['close'], color="skyblue")
 	fig1.plot(macd_df['date'], macd_df['sma_1'])
 	fig1.plot(macd_df['date'], macd_df['sma_2'])
 	fig1.plot(macd_df['date'], macd_df['sma_3'])
+
+	fig1.text(0.05, 0.65, f'BBP: {args.bbp}', transform=fig1.transAxes)
+	fig1.plot(bb_df['date'], bb_df['mean'], color="red", alpha=0.3)
+	fig1.plot(bb_df['date'], bb_df['bb_up1'], color="red", alpha=0.3)
+	fig1.plot(bb_df['date'], bb_df['bb_low1'], color="red", alpha=0.3)
+	fig1.plot(bb_df['date'], bb_df['bb_up2'], color="red", alpha=0.3)
+	fig1.plot(bb_df['date'], bb_df['bb_low2'], color="red", alpha=0.3)
+	fig1.fill_between(bb_df['date'], bb_df['bb_up1'], bb_df['bb_low1'], color="pink", alpha=0.3)
+	fig1.fill_between(bb_df['date'], bb_df['bb_up2'], bb_df['bb_low2'], color="pink", alpha=0.3)
 
 	fig2.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 	fig2.set_title("MACD")
@@ -69,6 +95,10 @@ def main():
  
 	fig.tight_layout()
 	plt.show()
+
+def main():
+	args = get_args()
+	process(args)
 
 if __name__ == '__main__':
     main()
